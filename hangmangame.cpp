748091e07d36460f18f4c8073b3cfcc6262c8d98
Zhong Lin <125393049+Zhonglin1231@users.noreply.github.com>
2023-04-08 18:50:05 +0800

    Add the graph of the hangman
    Add the graph of the hangman, only the main function is waiting to be finished.

--- hangmangame.cpp
+++ hangmangame.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 using namespace std;
 
+int number_of_chances;
+
 struct choose_word
 {
 	int database;
@@ -15,17 +17,36 @@ choose_word choose()
 	choose_word data;
 	cout << "Input wordguess set (1 for fruits, 2 for sports): " << endl;
 	cin >> data.database;
-	while (data.database != 1 && data.database != 2)
+	while (data.database != 1 && data.database!=2)
 	{
 		cout << "Input wordguess set (1 for fruits, 2 for sports): " << endl;
 		cin >> data.database;
 	}
 	//automatically set the seed
 	srand(time(NULL));
-	data.randword = choice[data.database - 1][rand() % 8];
+	data.randword = choice[data.database-1][rand() % 8];
 
 	return data;
 }
+
+void state(int number_of_chances) {
+	string countdown = { "|O/|\\|/\\"};
+	string current;
+	current = countdown.substr(0, n);
+	for (int i = 0; i < 9 - n; i++)
+	{
+		current.insert(current.length() - 1, " ");
+	}
+	cout << "+--------+" << endl;
+	cout <<" " << current[0] << "       | |"<<endl;
+	cout << " " << current[1] << "       | |" << endl;
+	cout << current[2] << current[3] << current[4] << "      | |"<<endl;
+	cout <<" " <<current[5] << "       | |\n";
+	cout << current[6] << " " << current[7] << "      | |" << endl;
+	cout << "         | |" << endl;
+	cout << " ________|_|___" << endl;
+}
+
 
 int main() {
 	// generate the random word
