ced96e6b7fa6c81fbe60eb3ceaa15aa836673b51
Zhong Lin <125393049+Zhonglin1231@users.noreply.github.com>
2023-04-22 02:57:13 +0800

    Adjust to run in visual studio

--- 128functions.h
+++ 128functions.h
@@ -1,6 +1,5 @@
 //128functions.h
-#ifndef 128FUNCTIONS_H
-#define 128FUNCTIONS_H
+
 
 #include <iostream>
 #include <string>
@@ -42,4 +41,3 @@ compress_combine_slide slide_down(int board[4][4]);
 bool main_128();
 
 
-#endif
