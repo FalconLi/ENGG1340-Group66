8dccc12a04674c9a7fd0fb929dc00383cb5af029
HKU fan <1527045331@qq.com>
2023-04-22 15:44:49 +0800

    Update matchnum.h

--- matchnum.h
+++ matchnum.h
@@ -1,11 +1,17 @@
 #pragma once
+
 #include <iostream>
+#include <cctype>
 #include <string>
+#include <sstream>
 #include <sstream>
 
 using namespace std;
 
 void show_rule();
+
 int mathgame();
+
 bool Isinteger(string n);
+
 int matchnum();
