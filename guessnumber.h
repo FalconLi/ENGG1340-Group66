517879ea0603fc7c96c674222908dd39276f54a5
Zhong Lin <125393049+Zhonglin1231@users.noreply.github.com>
2023-04-22 15:50:28 +0800

    Add guessnumber.h

---
+++ guessnumber.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+bool is_digits(string str);
+bool guessnumber();
