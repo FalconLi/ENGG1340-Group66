ba1e108204160af19f55a73406a92811f82bb8c5
Haiying (Arthur) Li <124905905+FalconLi@users.noreply.github.com>
2023-04-28 22:13:24 +0800

    Update guessnumber.cpp

--- guessnumber.cpp
+++ guessnumber.cpp
@@ -79,5 +79,8 @@ bool guessnumber() {
         cout << "\n" << timeuse(time(NULL), t1) << "\nGuess(a number 1-20) or exit(e): ";
         cin >> guess;
     }
+    cout << "\033[33mPress enter to return to the map ";
+    cin.ignore();
+    cout << "\033[0m";
     return guess == to_string(number);
 }
