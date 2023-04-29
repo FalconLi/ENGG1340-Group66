dc307d119cb7fad736c447805d418a25dd880db3
Haiying (Arthur) Li <124905905+FalconLi@users.noreply.github.com>
2023-04-29 21:50:03 +0800

    Update guessnumber.h

--- guessnumber.h
+++ guessnumber.h
@@ -1,6 +1,7 @@
 //guessnumber.h
 #ifndef GUESSNUMBER_H
 #define GUESSNUMBER_H
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -11,7 +12,6 @@ using namespace std;
 
 bool is_digits(string str);
 bool guessnumber();
-
 
 #endif
 
