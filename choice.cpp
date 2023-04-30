2e29399fe3bd0e6841575f01b32f40f4180406c1
Haiying (Arthur) Li <124905905+FalconLi@users.noreply.github.com>
2023-04-30 23:00:42 +0800

    Update choice.cpp

--- choice.cpp
+++ choice.cpp
@@ -620,7 +620,7 @@ bool choice(){
     //    cout<<"You failed...Errr..."<<endl;
     //    return false;
     //}
-    for (int itr = 0; itr < 15; itr++){
+    for (int itr = 0; itr < 20; itr++){
 		pause();
     }
     return true;
